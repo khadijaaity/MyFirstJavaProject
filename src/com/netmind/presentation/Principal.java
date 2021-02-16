002daf0e8997b6879390c091e0b0ce7e05545dab
khadija ait <khadijaaitychou@gmail.com>
2021-02-16 21:58:51 +0100

    git commit -m "MyFirstJavaProject"

---
+++ src/com/netmind/presentation/Principal.java
@@ -0,0 +1,11 @@
+package com.netmind.presentation;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Calculator calculator = new Calculator();
+		System.out.println("This is my first java project");
+	}
+
+}
