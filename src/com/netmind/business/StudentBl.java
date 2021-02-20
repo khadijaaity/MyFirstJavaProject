875739a8dbc084f7695067e06c56d5a350ae2a78
khadija ait <khadijaaitychou@gmail.com>
2021-02-20 13:07:37 +0100

    Three layer architecture implemented closes #1

---
+++ src/com/netmind/business/StudentBl.java
@@ -0,0 +1,12 @@
+package com.netmind.business;
+
+import com.netmind.model.Student;
+
+public class StudentBl {
+	// método público add
+	public boolean add(Student student) {
+		throw new UnsupportedOperationException(
+				"The file is not implemented yet");
+	}
+
+}
--- src/com/netmind/presentation/Principal.java
+++ src/com/netmind/presentation/Principal.java
@@ -1,11 +1,39 @@
 package com.netmind.presentation;
 
+import java.util.Scanner;
+
+import com.netmind.business.StudentBl;
+import com.netmind.model.Student;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Calculator calculator = new Calculator();
-		System.out.println("This is my first java project");
+
+		// Declarar el objeto e inicializar con
+		// el objeto de entrada estándar predefinido
+		Scanner scanner = new Scanner(System.in);
+		Student student = new Student();
+		StudentBl studentBl = new StudentBl();
+
+		// entrada de una cadena
+		String name = scanner.nextLine();
+		String surname = scanner.nextLine();
+
+		// Entrada de datos numéricos
+		int age = scanner.nextInt();
+
+		// calcular la media de edad
+		double average = scanner.nextDouble();
+
+		// Imprima los valores para verificar si la entrada
+		// fue obtenida correctamente.
+
+		System.out.println("Nombre: " + name);
+		System.out.println("Edad: " + surname);
+		System.out.println("Edad: " + age);
+		System.out.println("Promedio: " + average);
+
 	}
 
 }
