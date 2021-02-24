ee1fc885aba40fdf84466a5a62f769c396dbba60
khadija ait <khadijaaitychou@gmail.com>
2021-02-24 14:53:14 +0100

    Implemented add student in StudentDao closes #4

--- src/com/netmind/dao/StudentDao.java
+++ src/com/netmind/dao/StudentDao.java
@@ -1,12 +1,20 @@
 package com.netmind.dao;
 
+import java.util.ArrayList;
+
 import com.netmind.model.Student;
 
 public class StudentDao {
 
+	private static ArrayList<Student> studentList = null;
+
+	static {
+		studentList = new ArrayList<Student>();
+	}
+
 	public boolean add(Student student) {
-		throw new UnsupportedOperationException(
-				"The file is not implemented yet");
+		studentList.add(student);
+		return true;
 	}
 
 }
