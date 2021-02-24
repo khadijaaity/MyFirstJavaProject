439d1a8e27917150b8b098337579b5a3c906010c
khadija ait <khadijaaitychou@gmail.com>
2021-02-24 14:51:08 +0100

    Implemented StudentBl with add and calculateAge methods closes #3

--- src/com/netmind/business/StudentBl.java
+++ src/com/netmind/business/StudentBl.java
@@ -1,12 +1,26 @@
 package com.netmind.business;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+
+import com.netmind.dao.StudentDao;
 import com.netmind.model.Student;
 
 public class StudentBl {
-	// método público add
+
 	public boolean add(Student student) {
-		throw new UnsupportedOperationException(
-				"The file is not implemented yet");
+		StudentDao studentDao = new StudentDao();
+
+		student.setAge(calculateAge(student.getDateOfBirth()));
+
+		return studentDao.add(student);
 	}
 
+	private int calculateAge(Date dateOfBirth) {
+		Period edad = Period.between(dateOfBirth.toInstant()
+				.atZone(ZoneId.systemDefault()).toLocalDate(), LocalDate.now());
+		return edad.getYears();
+	}
 }
